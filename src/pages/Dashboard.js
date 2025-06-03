0cd4668e8e1ad3e1681dba995e8184eb43656367
HuseynYunisov <hyunisov54@gmail.com>
2025-06-03 18:12:22 +0400

    Create Dashboard.js

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to Book Cafe</h1>
+      <p>Announcements and highlights will appear here.</p>
+    </div>
+  );
+}
